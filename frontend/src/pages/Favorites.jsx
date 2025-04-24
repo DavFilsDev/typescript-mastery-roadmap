8af9616b7d50fd8ea5b3932db231c728b0525f69
DavFilsDev <miharisoadavidfils@gmail.com>
2025-04-24 10:10:14 +0300

    refactor(init): delete everything useless and rebuild the entire structure of project

--- frontend/src/pages/Favorites.jsx
+++
@@ -1,9 +0,0 @@
-import '../css/Favorites.css';
-
-function Favorites() {
-    return <div className="favorites-empty">
-        <h2>No favorite Movies yet</h2>
-        <p>Start adding movies to your favorites and they will appears here</p>
-    </div>
-}
-export default Favorites
