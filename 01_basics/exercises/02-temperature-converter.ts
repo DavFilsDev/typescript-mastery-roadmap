72dcd839f3be9717e5a06337b89d0207ca7c23eb
DavFilsDev <miharisoadavidfils@gmail.com>
2025-09-17 09:10:20 +0300

    test(exercises): add temperature converter exercise

---
+++ 01_basics/exercises/02-temperature-converter.ts
@@ -0,0 +1,25 @@
+/**
+ * EXERCISE 2: Temperature Converter
+ * 
+ * Create functions to convert between Celsius and Fahrenheit
+ */
+
+// TODO: Create a function that converts Celsius to Fahrenheit
+// Formula: (Celsius * 9/5) + 32 = Fahrenheit
+function celsiusToFahrenheit(celsius: number): number {
+  // Your code here
+  return 0;
+}
+
+// TODO: Create a function that converts Fahrenheit to Celsius
+// Formula: (Fahrenheit - 32) * 5/9 = Celsius
+function fahrenheitToCelsius(fahrenheit: number): number {
+  // Your code here
+  return 0;
+}
+
+// Test your functions
+console.log(celsiusToFahrenheit(0));    // Should be 32
+console.log(celsiusToFahrenheit(100));  // Should be 212
+console.log(fahrenheitToCelsius(32));   // Should be 0
+console.log(fahrenheitToCelsius(212));  // Should be 100
