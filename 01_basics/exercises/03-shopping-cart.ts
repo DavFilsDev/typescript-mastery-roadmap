d50092920763ef5d0ee889b573ae7ffd9fb08037
DavFilsDev <miharisoadavidfils@gmail.com>
2025-09-17 10:11:25 +0300

    test(exercises): add shopping cart exercise

---
+++ 01_basics/exercises/03-shopping-cart.ts
@@ -0,0 +1,34 @@
+/**
+ * EXERCISE 3: Shopping Cart
+ * 
+ * Work with arrays and objects
+ */
+
+// TODO: Create a type for a product with:
+// - id (number)
+// - name (string)
+// - price (number)
+// - inStock (boolean)
+
+
+// TODO: Create an array of 3-5 products
+
+
+// TODO: Create a function that:
+// - Takes a product array
+// - Returns only products that are in stock
+function getInStockProducts(products: any[]): any[] {
+  // Your code here
+  return [];
+}
+
+// TODO: Create a function that calculates the total price
+// of all products (only in stock ones)
+function calculateTotal(products: any[]): number {
+  // Your code here
+  return 0;
+}
+
+// Test your functions
+// Expected: Only show in-stock items
+// Expected total: Sum of in-stock product prices
