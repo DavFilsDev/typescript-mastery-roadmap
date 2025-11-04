aa3caf5e3568b966e5298b29fab6f9f6bf6c9106
DavFilsDev <miharisoadavidfils@gmail.com>
2025-11-04 09:05:42 +0300

    test(intermediate): add shape interfaces exercise

---
+++ 02-intermediate/exercises/04-shape-interfaces.ts
@@ -0,0 +1,27 @@
+/**
+ * EXERCISE 1: Shape Interfaces
+ * 
+ * Create interfaces for different shapes and calculate their area
+ */
+
+// TODO: Create an interface Shape with a common method getArea(): number
+
+
+// TODO: Create interfaces for:
+// - Rectangle (width: number, height: number)
+// - Circle (radius: number)
+// - Triangle (base: number, height: number)
+
+
+// TODO: Create a function that accepts any Shape and returns its area
+// function printArea(shape: Shape): void
+
+
+// TODO: Create an array of shapes and print all areas
+
+// Test data:
+// const shapes = [
+//   { width: 10, height: 5 },
+//   { radius: 7 },
+//   { base: 8, height: 6 }
+// ];
