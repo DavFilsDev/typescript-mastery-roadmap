475cf54606fd43438b91702362a2db72dd70ede8
DavFilsDev <miharisoadavidfils@gmail.com>
2025-11-12 08:28:54 +0300

    test(intermediate): add payment system with enums exercise

---
+++ 02-intermediate/exercises/08-enum-payment.ts
@@ -0,0 +1,34 @@
+/**
+ * EXERCISE 8: Payment System with Enums
+ * 
+ * Create a payment processing system using enums
+ */
+
+// TODO: Create an enum for PaymentMethod
+// - CreditCard, DebitCard, PayPal, BankTransfer, Crypto
+
+
+// TODO: Create an enum for PaymentStatus
+// - Pending, Processing, Completed, Failed, Refunded
+
+
+// TODO: Create an enum for Currency
+// - USD, EUR, GBP, JPY, BTC
+
+
+// TODO: Create an interface for Payment
+// - id: number
+// - amount: number
+// - method: PaymentMethod
+// - status: PaymentStatus
+// - currency: Currency
+// - createdAt: Date
+
+
+// TODO: Create a PaymentProcessor class that:
+// - Stores payments in an array
+// - Methods: createPayment, processPayment, refundPayment, getPaymentsByStatus
+// - Method to convert currency (use simple conversion rates)
+
+
+// Test your implementation
