ec8ada45b5b661eef73af795dc7926eb88573370
DavFilsDev <miharisoadavidfils@gmail.com>
2025-11-16 10:20:11 +0300

    test(intermediate): add vehicle rental exercise

---
+++ 02-intermediate/exercises/10-vehicle-rental.ts
@@ -0,0 +1,33 @@
+/**
+ * EXERCISE 10: Vehicle Rental System
+ * 
+ * Create a vehicle rental system using interfaces, classes, and enums
+ */
+
+// TODO: Create enum for VehicleType: Car, Motorcycle, Truck, Van
+
+
+// TODO: Create enum for FuelType: Petrol, Diesel, Electric, Hybrid
+
+
+// TODO: Create enum for RentalStatus: Available, Rented, Maintenance, Reserved
+
+
+// TODO: Create interface Vehicle with common properties:
+// - id, make, model, year, type, fuelType, dailyRate, status
+
+
+// TODO: Create specific vehicle interfaces that extend Vehicle:
+// - Car: doors, transmission, airConditioning
+// - Motorcycle: engineSize, hasSidecar
+// - Truck: cargoCapacity, axles
+
+
+// TODO: Create a VehicleRental class that:
+// - Maintains a fleet of vehicles
+// - Methods: addVehicle, removeVehicle, rentVehicle, returnVehicle
+// - Methods: getAvailableVehicles, getVehiclesByType, getVehiclesByFuel
+// - Method: calculateRentalCost(vehicle, days): number
+
+
+// Test your implementation
